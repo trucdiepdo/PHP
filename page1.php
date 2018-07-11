d62f6c058f2b0293558c1000b2f34c05189a8ca4
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-12 06:31:58 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,121 +1,42 @@
-<html>
+<!DOCTYPE html>
 <head>
-	<title>Assignment</title>
+<title>Insert data to PostgreSQL with php - creating a simple web application</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+li {listt-style: none;}
+</style>
 </head>
 <body>
-	<div id="main">
-		<div id="body">
-			<div>
-				<table border="1">
-					<tbody>
-						<tr>
-							<td>
-								<span><b>DANH SACH NHOM</b></span>
-							</td>
-							<td>
-								<div>
-									<a href="#">CAP NHAT</a>
-								</div>
-								<div>
-									<a href="#">DONG</a>
-								</div>
-							</td>							
-						</tr>
-					</tbody>
-				</table>
-				<br>
-				<table border="1">
-					<tbody>
-						<tr>
-							<td>SU DUNG</td>
-						</tr>
-						<tr>
-							<td>
-								<select>
-									<option>Use 1</option>
-								</select>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<br>
-			<!-- Show -->
-			
-			<div>
-				<div>
-						<table border="1">
-							<thead>
-								<tr>
-									<th>TEN NHOM</th>
-									<th>THU TU HIEN THI</th>
-									<th>XOA</th>
-									<th>SO LUONG LIEN KET</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>demo</td> 
-									<td>10</td>
-									<td> </td>
-									<td>1</td>
-								</tr>
-								<tr>
-									<td>demo_2</td>
-									<td>20</td> 
-									<td>
-										<img src="http://172.16.3.82:8082/assets/img/accept.gif">
-									</td>
-									<td>1</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				<br>
-			
-					<div>
-					<p>
-						<span><b>THONG TIN VAN PHONG BAN HANG THUOC VE NHOM DUOC CHON</b></span>
-					</p>
-					<div>
-						<table border="1">
-							<thead>
-								<tr>
-									<th>PHONG KINH DOAN<br>MA SO</th>
-									<th>TEN VP BAN HANG</th>
-									<th>TRINH DO CHUYEN MON</th>
-									<th>DIA CHI</th>
-									<th>NHAN VIEN BAN HANG</th>
-									<th>VI TRI THAM DU HOI NGHI BAN HANG</th>
-								</tr>
-							</thead>
-							<tbody id="officeInGroupResults">
-								<tr>
-									<td>e00002</td>
-									<td>demo</td>
-									<td>demo</td>
-									<td>Tphcm</td>
-									<td>duc2</td>
-									<td>demo</td>
-								</tr>
-							</tbody>
-						</table>
-						<input type="hidden" value="4"> 
-						<input type="hidden" value="2"> 
-						<input type="hidden" value="0">
-					</div>
-				</div>
-				
-			</div>
-		</div>
-	</div>
-	<footer>
-			<div>NGAY CAP NHAT : 2018/06/29 10:22:52 &emsp; CAP NHAT BOI : 0003
-				&emsp; TIEN-TT</div>
-		</footer>
+<h2>Enter information regarding book</h2>
+<ul>
+<form name="insert" action="book.php" method="POST" >
+<li>Book ID:</li><li><input type="text" name="bookid" /></li>
+<li>Author:</li><li><input type="text" name="author" /></li>
+<li>Publisher:</li><li><input type="text" name="publisher" /></li>
+<li>Date of publication:</li><li><input type="text" name="dop" /></li>
+<li>Price (USD):</li><li><input type="text" name="price" /></li>
+<li><input type="submit" /></li>
+<br><br>
+<select>
+	<option>Book 1</option>
+	<?php 
+	$db = pg_connect("host=localhost port=5432 dbname=training user=postgres password=have@tript0Singapore");
+	$ret = pg_query($db,"select * from M_GROUP_OFFICE");
+	while($row = pg_fetch_row($ret)){
+	    echo "<option>Use " .$row[1] ."</option>";
+	}
+	?>
+</select>
+</form>
+</ul>
 </body>
 </html>
-<?php 
-$db = pg_connect("host=172.16.3.82 port=5432 dbname=Training user=postgres password=have@tript0Singapore");
+<?php
+
+// $query = "INSERT INTO book VALUES ('".$_POST["bookid"]."',
+// '".$_POST["author"]."','".$_POST["publisher"]."',to_date('".$_POST["dop"]."','dd/MM/yyyy'),
+// '".$_POST["price"]."')";
+// var_dump($query);
+// $result = pg_query($query); 
 
 ?>
