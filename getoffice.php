4f4a243048bce7baed86fc7233ccc3a2f06e4a05
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-13 19:08:26 +0700

    Create getoffice.php

---
+++ getoffice.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_POST)) {
+    $db = pg_connect("host=172.16.3.82 port=5432 dbname=Training user=postgres password=have@tript0Singapore");
+    $q = $_POST['groupInforResults'];
+    $res3 = pg_query($db, 'select "M_GROUP_OFFICE"."GROUP_ID", "M_OFFICE"."OFFICE_CD", "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."QUALIFICATION", 
+                                        "M_OFFICE"."ADDRESS", "M_OFFICE"."PERSON", "M_OFFICE"."STATE"
+                            from  public."M_GROUP_OFFICE"
+                            inner join public."M_OFFICE"
+                            on "M_GROUP_OFFICE"."OFFICE_CD" = "M_OFFICE"."OFFICE_CD"
+                            where "M_GROUP_OFFICE"."GROUP_ID" = \''.$q.'\'
+                            group by "M_GROUP_OFFICE"."GROUP_ID", "M_OFFICE"."OFFICE_CD", "M_OFFICE"."PERSON", "M_OFFICE"."QUALIFICATION", 
+                            "M_OFFICE"."ADDRESS", "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."STATE"');
+while($row = pg_fetch_row($res3)) {
+    echo "<tr>
+									<td>$row[1]</td>
+									<td>$row[2]</td>
+									<td>$row[3]</td>
+									<td>$row[4]</td>
+									<td>$row[5]</td>
+									<td>$row[6]</td>
+								</tr>";
+}
+}
+?>
