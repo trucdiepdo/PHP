4e968e3106f0974b451a5ee725f8a6c7c5802eb5
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-18 18:47:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,119 @@
-<?php 
-$db = pg_connect("host=localhost port=5432 dbname=training user=postgres password=have@tript0Singapore");
+<?php
+$db = pg_connect("host=172.16.3.82 port=5432 dbname=Training user=postgres password=have@tript0Singapore");
 // if(!$db) {
 //     echo "Error : Unable to open database\n";
 // } else {
 //     echo "Opened database successfully\n";
 // }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="ja-jp">
 <head>
-	<title>Assignment</title>
+	<meta charset="utf-8">
+	<title>グループ一覧</title>
+	<meta http-equiv="Content-Language" content="ja">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="Pragma" content="no-cache">
+	<link rel="stylesheet" href="style.css">
+	<script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+	<script type="text/javascript">
+	$("#ddlGroup").ready(function(){
+			var value= $('#ddlGroup option:selected').attr('value');
+				$.ajax({
+				method:'POST',
+				url:"getUse.php",
+				data:{
+					"ddlGroup":value,
+				},
+				success:function(data){
+					$('#groupInforResults').html(data);
+				},
+				error: function() {
+					alert("Failed to load");
+				}	
+			});
+		});
+	$(function(){
+			$("#ddlGroup").change(function(){
+				var value= $('#ddlGroup option:selected').attr('value');
+					$.ajax({
+					method:'POST',
+					url:"getUse.php",
+					data:{
+						"ddlGroup":value,
+					},
+					success:function(data){
+						$('#groupInforResults').html(data);
+					},
+					error: function() {
+						alert("Failed to load");
+					}	
+				});
+			});
+			 	$('#infoTable').on('click', 'tbody tr', function(event) {
+		 		  $(this).css("background","#ffe7a0").siblings().css("background","");
+							$.ajax({
+							method:'POST',
+							url:"getOffice.php",
+							data:{
+								"groupInforResults":parseInt($(this).attr("id-get-office")),
+							},
+							success:function(data){
+								console.log(data);
+								$('#officeInGroupResults').html(data);
+							}	
+						});
+					});
+				$('#infoOfficeTable').on('click', 'tbody tr', function(event) {
+                		 $(this).css("background","#ffe7a0").siblings().css("background","");
+                		});
+});
+		function onClickedUpdate(){
+			window.location.href = 'update.php?groupflg=' + $('#ddlGroup option:selected').val();
+		}
+		function close_window() {
+			  if (confirm("閉じる?")) {
+			    close();
+			  }
+			}
+		</script>
 </head>
 <body>
 	<div id="main">
 		<div id="body">
 			<div>
-				<table border="1">
+				<table class="title titleGroupOffice">
 					<tbody>
-						<tr>
-							<td>
-								<span><b>グループ一覧</b></span>
+						<tr style="background-color: transparent">
+							<td class="formTitle b-none " style="width: 1030px;">
+								<span id="lblTitle"><b>グループ一覧</b></span>
 							</td>
-							<td>
-								<div>
-									<a id="btnUpdate" href="#">更新</a>
-									
+							<td class="btnGoto_Orange b-none">
+
+								<div id="UpdPlBtnUpdate" class="d-inline">
+									<a id="btnUpdate" href="#" onclick="onClickedUpdate();return false;" tabindex="1">更新</a>
 								</div>
-								<div>
-									<a id="btnClose" href="#" onclick="close_window();return false;">閉じる</a>
+								<div id="UpdPlBtnClose" class="d-inline">
+									<a id="btnClose" tabindex="2" href="#" onclick="close_window();return false;">閉じる</a>
 								</div>
-							</td>							
+							</td>					
 						</tr>
 					</tbody>
 				</table>
 				<br>
-				<table border="1" style="width: 250px">
+				<table class="table table-bordered" style="width: 250px">
 					<tbody>
 						<tr>
-							<td style="background-color: #bbddff">使用する</td>
+							<td style="background-color: #bbddff">用途</td>
 						</tr>
 						<tr style="background-color: white">
 							<td>
-								<select name="dllGroup" id="dllGroup" style="width: 230px" onchange = "selectChanged(this.value)">
+								<select name="ddlGroup" id="ddlGroup" style="width: 230px">
 									<?php 
-	                                 $ret = pg_query($db,'select distinct "GROUP_FLG" from public."M_GROUP_OFFICE" 
+	                                 $res1 = pg_query($db,'select distinct "GROUP_FLG" from public."M_GROUP" 
                                                             order by "GROUP_FLG"');
-	                                 while($row = pg_fetch_row($ret)){
-	                                 echo "<option value =''>Use " .$row[0] ."</option>";
+	                                 while($row = pg_fetch_row($res1)){
+	                                 echo "<option value ='$row[0]'>Use " .$row[0] ."</option>";
 	                                 }
 	                               ?>
 								</select>
@@ -57,9 +125,10 @@ $db = pg_connect("host=localhost port=5432 dbname=training user=postgres passwor
 			<br>
 			<!-- Show -->
 			
-			<div>
-				<div>
-						<table border="1">
+			<div id="GroupOffice">
+				<div id="GroupOfficeList">
+					<div id="UpdPlGroupOfficeList">
+						<table id="infoTable">
 							<thead>
 								<tr>
 									<th style="width: 900px;">グループ名</th>
@@ -68,43 +137,23 @@ $db = pg_connect("host=localhost port=5432 dbname=training user=postgres passwor
 									<th style="width: 100px;">所属件数</th>
 								</tr>
 							</thead>
-							<tbody>
-							<?php 
-							$ret = pg_query($db, 'select "M_GROUP"."GROUP_NM", "M_GROUP"."GROUP_ORDER", "M_GROUP"."DEL_FLG", count("M_GROUP_OFFICE"."OFFICE_CD") 
-	                                                     from public."M_GROUP" inner join public."M_GROUP_OFFICE"
-	                                                     on "M_GROUP"."GROUP_ID" = "M_GROUP_OFFICE"."GROUP_ID"
-	                                                     group by "M_GROUP"."GROUP_NM", "M_GROUP"."GROUP_ORDER", "M_GROUP"."DEL_FLG"');
-							while($row = pg_fetch_row($ret)){
-								echo 
-								"<tr>
-									<td>$row[0]</td>
-									<td>$row[1]</td> 
-									<td>"; ?>
-									<?php 
-									if($row[2] == 1)
-									       echo '<img src="https://www.zfshare.com/theme/Template-3/img/green_ticks.gif" width="20">';
-									else
-									    echo '';
-									?>
-							<?php 
-								echo "</td>
-								    <td>$row[3]</td>
-								</tr>";
-							}?>
+							<tbody id="groupInforResults">
 							</tbody>
 						</table>
+						</div>
 					</div>
 				<br>
 			
-					<div>
+					<div id="GroupOfficeDetail">
 					<p>
-						<span><b>◆選択したグループに属する営業所情報</b></span>
+						<span id="lblInfoDetailGroup"><b>◆選択したグループに属する営業所情報</b></span>
 					</p>
-					<div>
-						<table border="1">
+					<div id="UpdPlGroupOfficeDetail">
+						<table id="infoOfficeTable">
 							<thead>
 								<tr>
-									<th style="width: 90px;">営業所<br>コード</th>
+									<th style="width: 90px;">営業所<br> コード
+									</th>
 									<th style="width: 250px;">営業所名</th>
 									<th style="width: 120px;">資格</th>
 									<th style="width: 425px;">住所</th>
@@ -113,33 +162,17 @@ $db = pg_connect("host=localhost port=5432 dbname=training user=postgres passwor
 								</tr>
 							</thead>
 							<tbody id="officeInGroupResults">
-								<?php 
-								$res = pg_query($db, 'select "OFFICE_CD", "PERSON", "QUALIFICATION", "ADDRESS", "OFFICE_SNAME", "STATE"
-	                                                   from public."M_OFFICE"');
-								while($row = pg_fetch_row($res)) {
-								    echo "<tr>
-									<td>$row[0]</td>
-									<td>$row[1]</td>
-									<td>$row[2]</td>
-									<td>$row[3]</td>
-									<td>$row[4]</td>
-									<td>$row[5]</td>
-								</tr>";
-								}
-								?>
+							
 							</tbody>
 						</table>
-						<input type="hidden" value="4"> 
-						<input type="hidden" value="2"> 
-						<input type="hidden" value="0">
 					</div>
 				</div>
 				
 			</div>
 		</div>
-	</div>
-	<footer>
-			<div>更新日時 : 2018/06/29 10:22:52 &emsp; 更新者 : 0003
+		</div>
+	<footer id="footer">
+			<div class="float-right">更新日時 ： <?php echo date('Y/m/d H:i:s');?> &emsp; 更新者 ： 0003
 				&emsp; TIEN-TT</div>
 		</footer>
 </body>
