5237a0a93f70895cc61e1db167fdb595c0fff45a
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-20 18:58:08 +0700

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,29 +1,31 @@
 <?php
 include 'connect_postgresql.php';
 if(isset($_POST)){
-    
     $groupflg = $_POST["GroupFlg"];
     $data = $_POST["myData"];
-    var_dump($data);
-//     var_dump($groupflg);
+//     $remove = $_POST["Remove"];
+//     var_dump($remove);
     pg_query("BEGIN") or die("Could not start transaction\n");
     try {
         foreach($data as $key=>$value){
-            if($value["IsNew"] == 1){
-            $sql = 'insert into public."M_GROUP" ("GROUP_ID", "GROUP_NM", "GROUP_ORDER") values (\''.$value["GroupId"].'\',\''.$value["GroupNM"].'\,\''.$value["GroupOrder"].'\)';
-//             $query = "insert into public.\"M_GROUP\" (\"GROUP_ID\", \"GROUP_NM\", \"GROUP_ORDER\") values ('$value['GroupId']',\''.$value["GroupNM"].'\,\''.$value["GroupId"].'\)"
-            $res_insert = pg_query($sql);
-            }
-//             else if($value[IsNew] == 1){
-//                 $sql = 'update public."M_GROUP" set "GROUP_NM" = \''.$value["GroupNM"].'\, "GROUP_ORDER" = \''.$value["GroupOrder"].'\ where "GROUP_ID" = \''.$value["GroupId"].'\'';
-//                 $res_update = pg_query($sql);
-//             }
+            var_dump($value);
+            
+            // Add & insert row
+            $sql_insert = 'insert into public."M_GROUP" ("GROUP_ID", "GROUP_NM", "GROUP_ORDER","GROUP_FLG") values (\''.$value["GroupId"].'\',\''.$value["GroupNM"].'\',\''.$value["GroupOrder"].'\',\''.$groupflg.'\')';           
+            $res_insert = pg_query($sql_insert);
+            
+            // Update 
+            
+            
+            // Delete
+            $sql_delete = 'delete from public."M_GROUP" where "GROUP_ID" = \''.$value["GroupId"].'\'';
+            $res_delete = pg_query($sql_delete);
         }
         pg_query("COMMIT") or die("Transaction commit failed\n");
     } catch (Exception $e) {
         pg_query("ROLLBACK") or die("Transaction rollback failed\n");;
     }
     finally {
-        pg_close($sql);
+        pg_close();
     }
 }
