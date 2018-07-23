a3a2d3b681b224bb1c49df538372ad18fce37af7
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-23 10:15:20 +0700

    Create getUse.php

---
+++ assignment/getUse.php
@@ -0,0 +1,45 @@
+<?php
+include 'connect_postgresql.php';
+if (isset($_POST)) {
+    $groupid = $_POST['ddlGroup'];
+    $result  = pg_query('SELECT
+    "M_GROUP"."GROUP_ID",
+    "M_GROUP"."GROUP_NM",
+    "M_GROUP"."GROUP_ORDER",
+    "M_GROUP"."DEL_FLG",
+    COUNT( "M_GROUP_OFFICE"."OFFICE_CD" ) AS "DEM",
+    "M_GROUP"."GROUP_FLG"
+FROM
+    PUBLIC."M_GROUP"
+    left JOIN PUBLIC."M_GROUP_OFFICE" ON "M_GROUP"."GROUP_FLG" = "M_GROUP_OFFICE"."GROUP_FLG"
+    AND "M_GROUP"."GROUP_ID" = "M_GROUP_OFFICE"."GROUP_ID"
+WHERE
+    "M_GROUP"."GROUP_FLG" = \'' . $groupid . '\'
+GROUP BY
+    "M_GROUP"."GROUP_ID",
+    "M_GROUP"."GROUP_NM",
+    "M_GROUP"."GROUP_ORDER",
+    "M_GROUP"."DEL_FLG"
+ORDER BY
+    "M_GROUP"."GROUP_ORDER"');
+    
+    
+    while ($row = pg_fetch_row($result)) {
+        echo "<tr group-id='$row[0]' group-flg='$row[5]'>
+            <td style='text-align: left; word-wrap: break-word;'>$row[1]</td>
+            <td style='text-align: center;'>$row[2]</td>
+            <td style='text-align: center;'>";
+        ?>
+           <?php
+            if ($row[3] == 1)
+                echo '<img src="http://172.16.3.82:8082/assets/img/accept.gif">';
+            else
+                echo '';
+            ?>
+           <?php
+        echo "</td>
+             <td style='text-align: right;'>$row[4]</td>
+             </tr>";
+    }
+}
+?>
