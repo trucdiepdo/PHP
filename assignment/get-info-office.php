e26d37e169634367a5c8772416e1f5861a7bf666
trucdiepdo <trucdiep.do96@gmail.com>
2018-07-27 02:12:49 +0700

    up source

--- assignment/get-info-office.php
+++ assignment/get-info-office.php
@@ -2,18 +2,36 @@
 include 'connect_postgresql.php';
 $result = pg_query('select "PERSON", "STATE", "OFFICE_CD", "OFFICE_NAME", "QUALIFICATION", "OFFICE_SNAME", "PHONE", "ADDRESS"
                                         from public."M_OFFICE"');
-while ($row = pg_fetch_row($result)) {
-    echo "<tr>
-									<td><input type=\"checkbox\"></td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[0]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[1]</td>
-									<td style=\"text-align: left; word-wrap: break-word;\">$row[2]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[3]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[4]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[5]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[6]</td>
-									<td style=\"text-align: left; word-wrap: break-word;\">$row[7]</td>
-								</tr>";
+$result_kq = pg_query('select "M_GROUP_OFFICE"."GROUP_ID", "M_OFFICE"."OFFICE_CD", "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."QUALIFICATION", 
+                                        "M_OFFICE"."ADDRESS", "M_OFFICE"."PERSON"
+                            from  public."M_GROUP_OFFICE"
+                            inner join public."M_OFFICE"
+                            on "M_GROUP_OFFICE"."OFFICE_CD" = "M_OFFICE"."OFFICE_CD"
+                            where "M_GROUP_OFFICE"."GROUP_ID" = \''.$_GET["groupid"].'\' and "M_GROUP_OFFICE"."GROUP_FLG" = \''.$_GET["groupflg"].'\'');
+$chuoi = '';
+while ($row = pg_fetch_row($result_kq)) {
+    $chuoi = $chuoi . trim($row[1]).'-';
 }
+
+ while ($row = pg_fetch_row($result)) {
+     echo "<tr>";
+     if(strpos($chuoi, trim($row[1])) !== false) {
+         echo "<td><input type=\"checkbox\" checked></td>";
+     }
+     else {
+         echo "<td><input type=\"checkbox\"></td>";
+     }
+ 									
+
+ 			echo						"<td style=\" text-align: left; word-wrap: break-word;\">$row[0]</td>
+ 									<td style=\" text-align: left; word-wrap: break-word;\">$row[1]</td>
+ 									<td style=\"text-align: left; word-wrap: break-word;\">$row[2]</td>
+ 									<td style=\" text-align: left; word-wrap: break-word;\">$row[3]</td>
+ 									<td style=\" text-align: left; word-wrap: break-word;\">$row[4]</td>
+ 									<td style=\" text-align: left; word-wrap: break-word;\">$row[5]</td>
+ 									<td style=\" text-align: left; word-wrap: break-word;\">$row[6]</td>
+ 									<td style=\"text-align: left; word-wrap: break-word;\">$row[7]</td>
+ 								</tr>";
+ }
 
 ?>
--- assignment/show.php
+++ assignment/show.php
@@ -6,7 +6,15 @@
 	<meta http-equiv="Content-Language" content="ja">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="style.css">
+	<script
+  src="https://code.jquery.com/jquery-1.9.1.min.js"
+  integrity="sha256-wS9gmOZBqsqWxgIVgA8Y9WcQOa7PgSIX+rPA0VL2rbQ="
+  crossorigin="anonymous"></script>
 	<script type="text/javascript" src="jquery.session.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	
 </head>
 <body>
 	<div id="main">
@@ -65,13 +73,11 @@
 
 	<!-- 	<form name="form1" method="post" action="#" id="form1"></form> -->
 	<!-- --jquery, bootstrap-- -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	
 	<script>
 
 	$( document ).ready(function() {
-	    var url ="get-info-office.php?groupid="+get_param('groupid');
+	    var url ="get-info-office.php?groupid="+get_param('groupid')+"&"+"groupflg="+get_param('groupflg');
 	    $.get( url, function( data ) {
 			$("#groupInforResults").html( data );
 		});
@@ -95,12 +101,10 @@
 	 function onClickedUpdate() {
 		var arrOffices =[];
 		$( "#groupInforResults input:checkbox:checked" ).each(function() {
-			   var row = $(this).parent().parent().children()[3];
-			   var value = $(row).html();
-			   arrOffices.push($.trim(value));
-			    
+			  var row = $(this).parent().parent().children()[3];
+			  var value = $(row).html();
+			  arrOffices.push($.trim(value));			    
 		});
-
 	       var sendInfo = {
 	           GroupId: get_param('groupid'),
 	           GroupFLG: get_param('groupflg'),
