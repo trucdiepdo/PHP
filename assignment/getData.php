9923518a843277dab3aadf732ec2362b5e0e5862
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-07-30 18:29:03 +0700

    Update getData.php

--- assignment/getData.php
+++ assignment/getData.php
@@ -1,10 +1,9 @@
 <?php
+session_start();
 include 'connect_postgresql.php';
-if(isset($_POST)){
-    var_dump($_POST["myData"]);
-
+if(isset($_POST) && empty($_POST)){
     if(isset($_POST["myData"]) && empty($_POST["Remove"])){
-        
+        var_dump($_POST["myData"]);
         $groupflg = $_POST["GroupFlg"];
         $delflg = 0;
         $data = $_POST["myData"];
@@ -20,7 +19,7 @@ if(isset($_POST)){
                         $res_insert = pg_query($sql_insert);
             }
             
-                if(isset($value["IsNew"]) && $value["IsNew"] == 0){
+            if(isset($value["IsNew"]) && $value["IsNew"] == 0){
                  // Update
                     $sql_update = 'update public."M_GROUP" set "GROUP_NM" = \''.$value["GroupNM"].'\', "GROUP_ORDER" = \''.$value["GroupOrder"].'\' where "GROUP_ID" = \''.$value["GroupId"].'\'';
                     $res_update = pg_query($sql_update);
@@ -35,7 +34,7 @@ if(isset($_POST)){
         }
     } else if (isset($_POST["Remove"]) || isset($_POST["myData"])){
         $remove = $_POST["Remove"];
-        var_dump($_POST["Remove"]);
+        var_dump($remove);
         try {
             foreach ($remove as $key=>$value){
                 $sql_delete = 'delete from public."M_GROUP" where "GROUP_ID" = \''.$value.'\'';
@@ -49,7 +48,27 @@ if(isset($_POST)){
             pg_close();
         }
     }
+}elseif (isset($_SESSION["myData"]) && isset($_POST)){
+    
+    $myData = $_SESSION["myData"]["OfficeCds"];
+    $groupId =  $_SESSION["myData"]["GroupId"];
+    $groupFlg =  $_SESSION["myData"]["GroupFLG"];
+    
+    pg_query("BEGIN") or die("Could not start transaction\n");
+    try {
+        foreach($myData as $key=>$value){
+                
+            $sql_session = 'insert into public."M_GROUP_OFFICE" ("GROUP_ID", "GROUP_FLG", "OFFICE_CD")
+                                    values (\''.$groupId.'\',\''.$groupFlg.'\',\''.$value.'\')';
+            $res_session = pg_query($sql_session);
+            }
+        pg_query("COMMIT") or die("Transaction commit failed\n");
+    } catch (Exception $e) {
+        pg_query("ROLLBACK") or die("Transaction rollback failed\n");;
+    }
+    finally {
+        pg_close();
+    }
 }
-
 ?>
 
