85e31490693f4ef9465963ba15577f0298cf650e
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-08-03 17:48:19 +0700

    Update index.php

--- mchien/index.php
+++ mchien/index.php
@@ -1,23 +1,36 @@
 <?php 
 require_once 'smarty/libs/Smarty.class.php';
-// require_once 'smarty/libs/Database.class.php';
 require_once 'connect_postgresql.php';
-/*$smarty = new Smarty(  array('pdo_dsn' => 'postgres:dbname=Training;host=172.16.3.82',
-    'user' => 'postgres',
-    'password' => 'have@tript0Singapore',
-    'pdo_driver_options' => array() ) ); */
-$result = pg_query('select distinct "GROUP_FLG" from "M_GROUP"');
+
+$result_table1 = pg_query('select distinct "GROUP_FLG" from public."M_GROUP"');
+
+// $result_table2 = pg_query('SELECT "M_GROUP"."GROUP_ID", "M_GROUP"."GROUP_NM", "M_GROUP"."GROUP_ORDER", "M_GROUP"."DEL_FLG", COUNT("M_GROUP_OFFICE"."OFFICE_CD") AS "DEM", "M_GROUP"."GROUP_FLG"
+//                          FROM PUBLIC."M_GROUP" left JOIN PUBLIC."M_GROUP_OFFICE" ON "M_GROUP"."GROUP_FLG" = "M_GROUP_OFFICE"."GROUP_FLG" AND "M_GROUP"."GROUP_ID" = "M_GROUP_OFFICE"."GROUP_ID"
+//                          WHERE "M_GROUP"."GROUP_FLG" = "1"
+//                          GROUP BY "M_GROUP"."GROUP_ID", "M_GROUP"."GROUP_NM", "M_GROUP"."GROUP_ORDER", "M_GROUP"."DEL_FLG"
+//                          ORDER BY"M_GROUP"."GROUP_ORDER"');
+
+// $result   = pg_query('select "M_GROUP_OFFICE"."GROUP_ID", "M_OFFICE"."OFFICE_CD", "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."QUALIFICATION",
+//                                   "M_OFFICE"."ADDRESS", "M_OFFICE"."PERSON", "M_OFFICE"."STATE"
+//                            from  public."M_GROUP_OFFICE"
+//                            inner join public."M_OFFICE" on "M_GROUP_OFFICE"."OFFICE_CD" = "M_OFFICE"."OFFICE_CD"
+//                            where "M_GROUP_OFFICE"."GROUP_ID" = \'' . $groupid . '\' and "M_GROUP_OFFICE"."GROUP_FLG" = \'' . $groupflg . '\'
+//                            group by "M_GROUP_OFFICE"."GROUP_ID", "M_OFFICE"."OFFICE_CD", "M_OFFICE"."PERSON", "M_OFFICE"."QUALIFICATION",
+//                            "M_OFFICE"."ADDRESS", "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."STATE"');
+
 $arrayid = array();
 $index = 0;
-while ($row = pg_fetch_row($result)) {
-    $arrayid[$index] = $row[0];  
+while ($row = pg_fetch_row($result_table1)) {
+    $arrayid[$index] = "User ".$row[0];
     $index++;
-    //echo "Author: $row[0]  E-mail: $row[1]";
-    //echo "<br />\n";
 }
-$smarty = new Smarty();
 
-//$smarty->force_compile = true;
+// while ($row = pg_fetch_row($result_table2)) {
+//     $arrayid[$index] = $row[0];
+//     $index++;
+// }
+
+$smarty = new Smarty();
 $smarty->debugging = true;
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
@@ -25,10 +38,9 @@ $smarty->setCompileDir('smarty/templates_c/');
 $smarty->setConfigDir('smarty/configs/');
 $smarty->setCacheDir('smarty/cache/');
 $smarty->setTemplateDir('smarty/templates/');
-$smarty->assign("arrayid",$arrayid);
-// $smarty->assign('id', array(1,2,3,4,5));
-// $smarty->assign('names', array('bob','jim','joe','jerry','fred'));
 
+// Get "GROUP_FLG"
+$smarty->assign("arrayId",$arrayid);
+    
 $smarty->display('index.tpl');
-
 
