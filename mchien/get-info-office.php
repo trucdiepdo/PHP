d35af9aaef3d68658c6b492d5e58e767ff1f77ae
Đỗ Thị Trúc Diệp <39025019+trucdiepdo@users.noreply.github.com>
2018-08-06 18:09:40 +0700

    Update get-info-office.php

--- mchien/get-info-office.php
+++ mchien/get-info-office.php
@@ -1,19 +1,48 @@
 <?php
-include 'connect_postgresql.php';
-$result = pg_query('select "PERSON", "STATE", "OFFICE_CD", "OFFICE_NAME", "QUALIFICATION", "OFFICE_SNAME", "PHONE", "ADDRESS"
-                                        from public."M_OFFICE"');
-while ($row = pg_fetch_row($result)) {
-    echo "<tr>
-									<td><input type=\"checkbox\"></td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[0]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[1]</td>
-									<td style=\"text-align: left; word-wrap: break-word;\">$row[2]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[3]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[4]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[5]</td>
-									<td style=\" text-align: left; word-wrap: break-word;\">$row[6]</td>
-									<td style=\"text-align: left; word-wrap: break-word;\">$row[7]</td>
-								</tr>";
-}
+session_start();
+require_once 'smarty/libs/Smarty.class.php';
+require_once 'connect_postgresql.php';
 
+
+$result = pg_query('select "PERSON", "STATE", TRIM("OFFICE_CD"), "OFFICE_NAME", "QUALIFICATION", "OFFICE_SNAME", "PHONE", "ADDRESS"
+                                        from public."M_OFFICE"');
+
+$result_compare = pg_query('select "M_GROUP_OFFICE"."GROUP_ID", TRIM("M_OFFICE"."OFFICE_CD"), "M_OFFICE"."OFFICE_SNAME", "M_OFFICE"."QUALIFICATION",
+                                        "M_OFFICE"."ADDRESS", "M_OFFICE"."PERSON"
+                            from  public."M_GROUP_OFFICE"
+                            inner join public."M_OFFICE"
+                            on "M_GROUP_OFFICE"."OFFICE_CD" = "M_OFFICE"."OFFICE_CD"
+                            where "M_GROUP_OFFICE"."GROUP_ID" = \''.trim($_GET["groupid"]).'\' and "M_GROUP_OFFICE"."GROUP_FLG" = \''.trim($_GET["groupflg"]).'\'');
+
+$array = array();
+$array1 = array();
+$array2 = array();
+$string = '';
+$string_session = '';
+$index = 0;
+// Check the existence of session
+if(isset($_SESSION['session_'.$_GET["groupid"].'-'.$_GET["groupflg"]])){
+    $string_session = $_SESSION['session_'.$_GET["groupid"].'-'.$_GET["groupflg"]];
+    if(trim($string_session) != ""){
+        $array2 = preg_split("/,/" , $string_session);
+    }
+}
+$index = 0;
+// Take all the values row from the database
+while ($row = pg_fetch_row($result_compare)) {
+    if(in_array(trim($row[1]), $array2) == false){
+        $array2[count($array2)] = $row[1];
+    }
+}
+$index = 0;
+while ($row = pg_fetch_row($result)) {
+    $array[$index] = $row;
+    $index++;
+}
+$smarty = new Smarty();
+$smarty->setTemplateDir('smarty/templates/');
+
+$smarty->assign('array1',$array2);
+$smarty->assign('row',$array);
+$smarty->display('get-info-office.tpl');
 ?>
